32bb35094cf903ad4d90c7f8196b0114d52ea459
Rohit Agrawalla <roagrawa@microsoft.com>
2020-07-10 00:11:20 -0700

    Added pcost

--- Work/Scratch.py
+++ Work/Scratch.py
@@ -9,4 +9,36 @@ for pt in doc.findall('.//pt'):
 
 
 
+with open('Work/Data/portfolio.csv', 'rt') as f:
+    data = f.read()
+
+with open('Work/Data/portfolio.csv', 'rt') as f:
+    for line in f:
+        print(line, end='')
+
+print(data)
+
+
+import csv
+
+with open('Work/Data/portfolio.csv') as f:
+    rows = csv.reader(f)
+    
+    for row in rows:
+        print(row)
+
+
+from pprint import pprint
+
+
+records = []  # Initial empty list
+
+with open('Work/Data/portfolio.csv', 'rt') as f:
+    next(f) # Skip header
+    for line in f:
+        row = line.split(',')
+        records.append((row[0], int(row[1]), float(row[2])))
+
+
+pprint(records)
 
--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,29 @@
 # pcost.py
 #
 # Exercise 1.27
+
+import sys
+import csv
+
+
+def portfolio_cost(filename):
+    '''Computes the total cost (shares*price) of a portfolio file'''
+    total_cost = 0.0
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            nshares = int(row[1])
+            price = float(row[2])
+            total_cost += nshares * price
+    return total_cost
+
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Work/Data/portfolio.csv'
+
+totalCost = portfolio_cost(filename)
+print(f'Total Cost {totalCost}')
